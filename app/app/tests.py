abc361cc3fc6f4d6c00158738883b981b5524ca3
maduka paul <139281123+iam-madukapaul@users.noreply.github.com>
2025-03-16 14:44:03 +0100

    Update postgreSQL adapter

--- app/app/tests.py
+++ app/app/tests.py
@@ -3,12 +3,12 @@
 from django.test import SimpleTestCase
 from app import calc
 
+
 class CalcTests(SimpleTestCase):
     """Test add numbers together."""
     def test_add_numbers(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-
 
     """Test subtract numbers."""
     def test_subtract_numbers(self):
